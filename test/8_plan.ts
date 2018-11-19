c2a39bf5b2be1ffdbceeec2f14d0c6ec80f44796
middletone <middletone@science.regn.net>
2018-11-19 21:58:30 +0000

    Simply plan example

--- test/8_plan.ts
+++ test/8_plan.ts
@@ -1,28 +1,16 @@
 import test from 'ava';
-import * as Config from 'config';
-
-function wrap(input1: number, input2: number ): any {
-     if (Config.get<string>('version') === '1') {
-         return { foo: input1, dog: input2};
-     } else {
-         return { foo: input1, cat: input2};
-     }
-}
+import { add, multiply } from '../src/functions';
 
 /**
  * Use plan to make sure your test is asserting everything it intends to.
  */
-test('Sets dog or cat', async t => {
+test('Two assertions', async t => {
     t.plan(2);
 
-    const wrapped = wrap(3, 6);
-    t.is(wrapped.foo, 3);
-
-    if (wrapped.dog) {
-        t.is(wrapped.dog, 6);
-    }
-
-    if (wrapped.cat) {
-        t.is(wrapped.cat, 6);
-    }
+    t.is(add(2, 3), 5);
+    t.is(multiply(2, 3), 6);
 });
+
+/**
+ * See https://github.com/avajs/ava/blob/master/docs/recipes/when-to-use-plan.md
+ */
