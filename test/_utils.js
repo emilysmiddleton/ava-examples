4708323c4f1239c3fb294fb980e15d5a884d11b7
middletone <middletone@science.regn.net>
2018-11-19 10:47:36 +0000

    Remove .js and .js.map files

--- test/_utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function delay() {
-    return new Promise(resolve => setTimeout(resolve, 1000));
-}
-exports.delay = delay;
-//# sourceMappingURL=_utils.js.map
